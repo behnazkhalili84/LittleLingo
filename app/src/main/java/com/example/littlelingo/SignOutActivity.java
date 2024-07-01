cde1920ecf1eb5a256a52a82101beccde886ee18
Kawtharmashkour <kawtharmashkour@yahoo.com>
2024-07-01 12:07:57 -0400

    solve navigation drawer

---
+++ app/src/main/java/com/example/littlelingo/SignOutActivity.java
@@ -0,0 +1,32 @@
+package com.example.littlelingo;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_out);
+
+        FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(SignOutActivity.this, SignInActivity.class);
+                   startActivity(intent);
+                   finish();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

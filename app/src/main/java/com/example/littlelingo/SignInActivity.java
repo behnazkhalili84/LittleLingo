543c6581291a4da8c163c50024a2526b24bfb5fc
Kawtharmashkour <kawtharmashkour@yahoo.com>
2024-07-03 09:48:13 -0400

    Forgot password by send email link from firebase

--- app/src/main/java/com/example/littlelingo/SignInActivity.java
+++ app/src/main/java/com/example/littlelingo/SignInActivity.java
@@ -1,15 +1,22 @@
 package com.example.littlelingo;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,12 +24,19 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.littlelingo.ui.user.AuthViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
+    ProgressDialog loadingBar;
     private AuthViewModel authViewModel;
+    private FirebaseAuth mAuth;
     private EditText userNameEditText, passwordEditText;
+    private MaterialButton forgetpass;
     private MaterialButton textButtonSignUp;
     private Button signInButton;
 
@@ -41,6 +55,7 @@ authViewModel.signOut();
         userNameEditText = findViewById(R.id.UserName);
         passwordEditText = findViewById(R.id.password);
         signInButton = findViewById(R.id.signInButton);
+
         // from material design for better error handling
         userNameInputLayout = findViewById(R.id.userNameInputLayout);
         passwordInputLayout = findViewById(R.id.passwordInputLayout);
@@ -78,6 +93,17 @@ authViewModel.signOut();
             }
         });
 
+        //Forgot Password
+        mAuth = FirebaseAuth.getInstance();
+        forgetpass=findViewById(R.id.textForgotPassword);
+        forgetpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRecoverPasswordDialog();
+            }
+        });
+
+        // Sign UP
         textButtonSignUp = findViewById(R.id.textButtonSignUp);
 
         textButtonSignUp.setOnClickListener(new View.OnClickListener() {
@@ -110,4 +136,69 @@ authViewModel.signOut();
             return insets;
         });
     }
+
+    private void showRecoverPasswordDialog() {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Recover Password");
+        LinearLayout linearLayout=new LinearLayout(this);
+        final EditText emailet= new EditText(this);
+
+        // write the email using which you registered
+        emailet.setText("Email");
+        emailet.setMinEms(16);
+        emailet.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        linearLayout.addView(emailet);
+        linearLayout.setPadding(10,10,10,10);
+        builder.setView(linearLayout);
+
+        // Click on Recover and a email will be sent to your registered email id
+        builder.setPositiveButton("Recover", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String email=emailet.getText().toString().trim();
+                beginRecovery(email);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+    private void beginRecovery(String email) {
+        loadingBar=new ProgressDialog(this);
+        loadingBar.setMessage("Sending Email....");
+        loadingBar.setCanceledOnTouchOutside(false);
+        loadingBar.show();
+
+        // calling sendPasswordResetEmail
+        // open your email and write the new
+        // password and then you can login
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                loadingBar.dismiss();
+                if(task.isSuccessful())
+                {
+                    // if isSuccessful then done message will be shown
+                    // and you can change the password
+                    Toast.makeText(SignInActivity.this,"Done sent",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(SignInActivity.this,"Error Occurred",Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                loadingBar.dismiss();
+                Toast.makeText(SignInActivity.this,"Error Failed",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
